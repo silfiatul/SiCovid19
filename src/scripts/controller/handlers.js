8f051433de754964651eee35d229a8bbd8c236fc
AkbarHusein <muhakbarhusein5@gmail.com>
2022-05-24 07:59:18 +0700

    Express.js init

---
+++ src/scripts/controller/handlers.js
@@ -0,0 +1,5 @@
+const home = (req, res) => {
+  res.send('Hallo CPGS-18 :)');
+};
+
+module.exports = { home };
---
+++ src/scripts/global/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  HOSTNAME: 'localhost',
+  PORT: 3000,
+};
+
+module.exports = CONFIG;
