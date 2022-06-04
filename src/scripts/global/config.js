fd239f1bbbf71c6fba636b459f56eb44598c4d69
AkbarHusein <muhakbarhusein5@gmail.com>
2022-06-04 12:07:31 +0700

    change base url covid api

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -4,6 +4,7 @@ const CONFIG = {
   BASE_URL_COVID: 'https://data.covid19.go.id/public/api',
   BASE_URL_VAKSIN: 'https://api.vaksinasi.id',
   CACHE_NAME: 'SiCovid19-V1',
+  CURK: '../utils/curl-helper.php',
 };
 
 module.exports = CONFIG;
